36fdb2b67bb84ca763a7eb944fd51f7efa201c0d
wmj <work@lmm.com>
2019-02-18 11:37:29 +0800

    ad

--- routes/web_routes.go
+++ routes/web_routes.go
@@ -1552,4 +1552,5 @@ func WebRegister(r *gin.Engine, eventHandler *event.EventHandler) {
 	getAllMenuInfo(r, eventHandler)              //获取所有菜单信息
 	updateWebUserPwd(r, eventHandler)            //修改web用户密码,不需要权限检测
 	getCommissionStatistics(r, eventHandler)     //佣金分成统计
+	getAvailableGoodsClass(r, eventHandler)
 }
