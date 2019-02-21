05b0571e9d0e500e4008457183bcf16431563203
wmj <work@lmm.com>
2019-02-21 11:06:23 +0800

    add

--- datastruct/sql.go
+++ datastruct/sql.go
@@ -569,6 +569,6 @@ type SaveUserGetGoldPoster struct {
 }
 
 type TmpTest struct {
-	Id     int `xorm:"not null pk autoincr INT(11) comment '自增编号' "`
-	UserId int `xorm:"not null INT(11) comment '用户编号' "`
+	Id     int `xorm:"not null pk autoincr INT(11) comment('自增编号')"`
+	UserId int `xorm:"not null INT(11) comment('用户编号')"`
 }
